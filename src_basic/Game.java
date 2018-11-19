783f3956388011172e4e66c57fdac7c1ab63c8e7
rob <robin.navarro@etu.u-bordeaux.fr>
2018-11-19 18:25:19 +0100

    Add some lines

--- src_basic/Game.java
+++ src_basic/Game.java
@@ -1,5 +1,15 @@
 package src_basic;
 
-class Game {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
 	
+	public void start(Stage stage) {
+		stage.setTitle("Andromeda");
+		stage.show();
+	}
 }
---
+++ src_basic/Model/StarShip.java
@@ -0,0 +1,7 @@
+package src_basic.Model;
+
+import javafx.geometry.Point2D;
+
+public class StarShip {
+	private Point2D position;
+}
