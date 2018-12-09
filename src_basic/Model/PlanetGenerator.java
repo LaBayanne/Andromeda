da30d07d6eef73553a431446adad70d7f08826de
chocorion <chocorion@gmail.com>
2018-12-09 16:27:16 +0100

    Ajout de la fonction isValidPlanet dans PlanetGenerator

--- src_basic/Model/PlanetGenerator.java
+++ src_basic/Model/PlanetGenerator.java
@@ -1,6 +1,9 @@
 package src_basic.Model;
 
+import java.util.ArrayList;
 import java.util.Random;
+
+import javafx.geometry.Point2D;
 
 public class PlanetGenerator {
 	private int maxSize;
@@ -32,5 +35,48 @@ public class PlanetGenerator {
 	
 	private int getRandom(int min, int max) {
 		return this.rand.nextInt(max - min + 1) + min;
+	}
+	
+	private boolean isValidPlanet(int originX, int originY, int radius, ArrayList<Planet> planets) {
+		if (originX - radius <= 0)	return false;
+		if (originY - radius <= 0)  return false;
+		
+		if (originX + radius >= this.windowWidth) 	return false;
+		if (originY + radius >= this.windowHeight)	return false;
+		
+		for (Planet p: planets) {
+			double distance = p.getOrigin().distance(originX, originY);
+			double planetTotalSize = p.getRadius() + radius;
+			
+			double distanceBetweenPlanet = distance - planetTotalSize;
+			
+			//Distance min : 20% sum of the two radius
+			if (distanceBetweenPlanet < 0.2 * (p.getRadius() + radius)) {
+				return false;
+			}
+			
+		}
+		return true;
+	}
+	
+	public ArrayList<Planet> generate() {
+		ArrayList<Planet> planetList = new ArrayList<>();
+		
+		int nbPlanet = getRandom(this.nbMin, this.nbMax);
+		int originX, originY;
+		int radius;
+		
+		for (int i = 0; i < nbPlanet; i++) {
+			//public Planet(Point2D origin, double radius, double productionSpeed, int owner)
+			do {
+				originX = this.getRandom(50, this.windowWidth);
+				originY = this.getRandom(50, this.windowHeight);
+				
+				radius = this.getRandom(this.minSize, this.maxSize);
+				
+				
+			}
+		}
+		return planetList;
 	}
 }
