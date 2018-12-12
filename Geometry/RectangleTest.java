db6de663246ca2d35bb6fc4dc813c5de2dbf4de5
chocorion <chocorion@gmail.com>
2018-12-12 16:15:18 +0100

    Ajout de points basiques

---
+++ Geometry/RectangleTest.java
@@ -0,0 +1,20 @@
+package Geometry;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RectangleTest {
+
+	@Test
+	void testCollision() {
+		Rectangle r = new Rectangle(10, 10, 10, 10);
+		
+		assert(r.collision(new Point(10, 10)));
+		assert(r.collision(new Point(20, 20)));
+		
+		assert(!r.collision(new Point(0, 10)));
+		assert(!r.collision(new Point(11, 21)));
+	}
+
+}
