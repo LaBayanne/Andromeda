022b6e23a6f77001b588d7872fbb239ad0a40dfa
LaBayanne <yannblanchet@orange.fr>
2018-12-13 13:14:54 +0100

    Clean Controller

--- src_basic/Model/Scene/Scenery.java
+++ src_basic/Model/Scene/Scenery.java
@@ -2,6 +2,9 @@ package src_basic.Model.Scene;
 
 import java.util.ArrayList;
 
+/**
+ * This interface represents a scene in game. Scenery because Scene is already taken by javafx.
+ */
 public interface Scenery {
         public boolean tick(double delta);
         public void mouseClicked(int button, double x,double y, ArrayList<String> buttonOptions);
