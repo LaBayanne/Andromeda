23376bca0e7f2400b75fd5e13d5910b84d743fdf
chocorion <chocorion@gmail.com>
2018-12-13 17:01:46 +0100

    Ajout de documentation

--- src_basic/Geometry/Point.java
+++ src_basic/Geometry/Point.java
@@ -12,20 +12,37 @@ public class Point implements Serializable {
 	private double x;
 	private double y;
 	
+	/**
+	 * Default constructor
+	 * @param x the x value 
+	 * @param y the y value
+	 */
 	public Point(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
 	
+	/**
+	 * Constructor by copy.
+	 * @param p Point to copy.
+	 */
 	public Point(Point p) {
 		this(p.getX(), p.getY());
 	}
 	
+	/**
+	 * Basic constructor
+	 */
 	public Point() {
 		this(0, 0);
 	}
 	
-	
+	/**
+	 * Calculate distance between point and coord
+	 * @param x	the x value
+	 * @param y	the y value
+	 * @return	the distance
+	 */
 	public double distance(double x, double y) {
 		double dx = this.x - x;
 		double dy = this.y - y;
@@ -36,30 +53,66 @@ public class Point implements Serializable {
 		return Math.sqrt(dx + dy);
 	}
 	
+	/**
+	 * Move point
+	 * @param dx	x deplacement
+	 * @param dy	y deplacement
+	 */
 	public void translate(double dx, double dy) {
 		this.x += dx;
 		this.y += dy;
 	}
 	
+	/**
+	 * Calculate angle between two point
+	 * @param p	the second point
+	 * @return
+	 */
 	public double angle(Point p) {
 		return Math.toDegrees(Math.atan2(p.getY() - this.y, p.getX() - this.x));
 	}
 	
+	/**
+	 * Calculate disctance between two points
+	 * @param p	the other point
+	 * @return
+	 */
 	public double distance(Point p) {
 		return distance(p.getX(), p.getY());
 	}
 	
-	public double getX()	{ return this.x; }
-	public double getY()	{ return this.y; }
+	/**
+	 * @return x coord
+	 */
+	public double getX() { 
+		return this.x;
+	}
 	
+	/**
+	 * @return y coord
+	 */
+	public double getY() { 
+		return this.y; 
+	}
+	
+	/**
+	 * @param x the new value for x.
+	 */
 	public void setX(double x) {
 		this.x = x;
 	}
 	
+	/**
+	 * @param y the new value for y.
+	 */
 	public void setY(double y) {
 		this.y = y;
 	}
 	
+	/**
+	 * Set point to specific position
+	 * @param p the new position of the point
+	 */
 	public void set(Point p) {
 		setX(p.getX());
 		setY(p.getY());
